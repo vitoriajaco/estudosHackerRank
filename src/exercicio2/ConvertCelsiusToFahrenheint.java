1fd5753369ded6fc39af9ac5f5565d0667a39b47
vitoriajaco <vitoriajaco@gmail.com>
2023-09-26 20:04:16 -0300

    Opçao 2 refatorada

--- src/exercicio2/ConvertCelsiusToFahrenheint.java
+++ src/exercicio2/ConvertCelsiusToFahrenheint.java
@@ -7,19 +7,13 @@ public class ConvertCelsiusToFahrenheint {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite a temperatura em Celsius: ");
-
-        double C = sc.nextDouble();
-        double F = 9.0 * C / 5.0 + 32.0;
-        System.out.printf("Esta temperatura " + C + "°C em Fahrentheint é de: %.1f%n", F); // %.1f%n corresponde a um valor de ponto flutuante. Sera inserido um valor com 1 casa decimal
-        System.out.println("Deseja repetir (s/n)? ");
-        char resp = sc.next().charAt(0); //O charAt(0) serve para pegar o primeiro caracter da palavra pois corresponde ao index
+        char resp = 's';
 
         while (resp != 'n') {
             System.out.print("Digite a temperatura em Celsius: ");
 
-            C = sc.nextDouble();
-            F = 9.0 * C / 5.0 + 32.0;
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
             System.out.printf("Esta temperatura " + C + "°C em Fahrentheint é de: %.1f%n", F); // %.1f%n corresponde a um valor de ponto flutuante. Sera inserido um valor com 1 casa decimal
             System.out.println("Deseja repetir (s/n)? ");
             resp = sc.next().charAt(0); //O charAt(0) serve para pegar o primeiro caracter da palavra pois corresponde ao index
