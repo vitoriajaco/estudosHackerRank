b877fd45fe82a82f6ae2a340d5ead4ca779f0bca
vitoriajaco <vitoriajaco@gmail.com>
2023-09-26 21:35:09 -0300

    Subindo codigo com erro de loop

--- src/exercicio1/Solution.java
+++ src/exercicio1/Solution.java
@@ -1,57 +1,32 @@
 package exercicio1;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
+
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
 
 public class Solution {
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Digite um numero: ");
+
         int  N = scanner.nextInt();
 
-        while (N >= 1 && N <= 100) {
-
-        }
-        System.out.println("Numero invalido: ");
-
-
-          /*  if ((N % 2 == 0 && N >= 2 && N <= 5)) { //se essa condição for V executa, senao executa a de baixo
-                System.out.println("validação 1 ");
-
-            }
-        } else
-            System.out.println("Numero invalido: ");
+        while (true) {
             System.out.println("Digite um numero: ");
-            N =  scanner.nextInt();
+            if (N >= 1 && N <= 100) {
 
-*/
+                if ((N % 2 == 0 && N >= 2 && N <= 5) || N > 20) {
+                    System.out.println("Not Weird");
+                } else if (N % 2 == 1 || (N % 2 == 0 && N >= 6 && N <= 20)) {
+                    System.out.println("Weird");
+                } else {
+                    System.out.println("Número fora do intervalo permitido (1 a 100).");
+                    break;
+
+
+                }
+            }
+        }
     }
 }
-
-
-/*
-        if (N % 2 == 1) {
-            System.out.println("Weird");
-
-        } else if (N % 2 == 0 && N >= 2 && N <=5){
-            System.out.println("Not Weird");
-
-        } else if (N % 2 == 0 && N >= 6 && N <=20){
-            System.out.println("Weird");
-
-        } else if (N % 2 == 0 && N >20 ){
-            System.out.println("Not Weird");
-
-        }
-
-        System.out.println("Entrou aqui");
-
-    }
-}*/
